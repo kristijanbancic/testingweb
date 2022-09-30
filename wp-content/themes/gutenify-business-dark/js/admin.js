5277aca69be6bcc4dbc1a7958908e539f6903bd5
kristijanbancic <76695964+kristijanbancic@users.noreply.github.com>
2022-09-30 19:22:05 +0200

    new

---
+++ wp-content/themes/gutenify-business-dark/js/admin.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var _wp = wp,
+    apiFetch = _wp.apiFetch;
+jQuery(function ($) {
+  var redirectToKitPage = function redirectToKitPage(res) {
+    // if( res?.status && 'active' === res.status ) {
+    window.location = "".concat(window.gutenify_business_dark.gutenify_kit_gallery); // }
+  }; // Activate Gutenify.
+
+
+  $(document).on('click', '.gutenify-business-dark-activate-gutenify', function () {
+    $(this).html('<span class="dashicons dashicons-update"></span> Loading...').addClass('gutenify-business-dark-importing-gutenify');
+    apiFetch({
+      path: '/wp/v2/plugins/gutenify/gutenify',
+      method: 'POST',
+      data: {
+        "status": "active"
+      }
+    }).then(function (res) {
+      redirectToKitPage(res);
+    })["catch"](function () {
+      redirectToKitPage({});
+    });
+  });
+  $(document).on('click', '.gutenify-business-dark-install-gutenify', function () {
+    $(this).html('<span class="dashicons dashicons-update"></span> Loading...').addClass('gutenify-business-dark-importing-gutenify');
+    apiFetch({
+      path: '/wp/v2/plugins',
+      method: 'POST',
+      data: {
+        "slug": "gutenify",
+        "status": "active"
+      }
+    }).then(function (res) {
+      redirectToKitPage(res);
+    })["catch"](function () {
+      redirectToKitPage({});
+    });
+  });
+});
